2568d37aafa12730d57cd9f88545988b2054f4c5
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-14 13:36:28 -0300

    Se aplicado, este commit irá ajustar o layout para utilizar o widget Stack

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -44,69 +44,77 @@ class _HomeState extends State<Home> {
         title: Text("Álcool ou Gasolina"),
         backgroundColor: Colors.blue,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(32),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: 32),
-                child: Image.asset("assets/images/logo.png"),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  "Saiba qual a melhor opção para abastecimento do seu carro.",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.all(32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 32),
+                      child: Image.asset("assets/images/logo.png"),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "Saiba qual a melhor opção para abastecimento do seu carro.",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Preço álcool, ex.: 1.59",
+                      ),
+                      style: TextStyle(fontSize: 22),
+                      controller: _controllerAlcool,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Preço gasolina, ex.: 3.59",
+                      ),
+                      style: TextStyle(fontSize: 22),
+                      controller: _controllerGasolina,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: ElevatedButton(
+                        onPressed: _calcular,
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blue)),
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Center(
+                        child: Text(
+                          _textoResultado,
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Preço álcool, ex.: 1.59",
-                ),
-                style: TextStyle(fontSize: 22),
-                controller: _controllerAlcool,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Preço gasolina, ex.: 3.59",
-                ),
-                style: TextStyle(fontSize: 22),
-                controller: _controllerGasolina,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: ElevatedButton(
-                  onPressed: _calcular,
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.blue)),
-                  child: Text(
-                    "Calcular",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Center(
-                  child: Text(
-                    _textoResultado,
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              )
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
